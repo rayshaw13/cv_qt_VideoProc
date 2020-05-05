ba3c86a7ac684e6314fb9d9aad35536ceb48ad10
rayshaw13 <foryou031214@163.com>
2020-05-05 15:20:46 +0800

    新建图像处理类XImagePro，定义多个函数，完成亮度对比度调节函数

---
+++ XVideoEdit/XImagePro.cpp
@@ -0,0 +1,24 @@
+#include "XImagePro.h"
+
+
+
+XImagePro::XImagePro()
+{
+}
+
+
+XImagePro::~XImagePro()
+{
+}
+
+void XImagePro::Set(cv::Mat mat1, cv::Mat mat2) {
+	if (mat1.empty())return;
+	this->src1 = mat1;
+	this->src2 = mat2;
+	this->src1.copyTo(des);
+}
+
+void XImagePro::Gain(double bright, double contrast) {
+	if (des.empty())return;
+	des.convertTo(des, -1, contrast, bright);
+}
